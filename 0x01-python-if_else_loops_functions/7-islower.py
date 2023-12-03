3e1b7c13c14a987d40258869ebfde0841de11e0d
Method Mkoma <gershommethod02@gmail.com>
2023-12-03 10:00:30 +0300

    Task 7

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
+    if (c == '') or (c == '7'):
+        return
     for i in range(33, 123):
         if (i >= 97) and (i < 123):
             if (chr(i) == c):
