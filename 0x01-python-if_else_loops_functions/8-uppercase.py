aad327530ed0e6cf1d589f888d005d7fa6bcfd99
Method Mkoma <gershommethod02@gmail.com>
2023-12-03 11:10:26 +0300

    Task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -5,9 +5,7 @@ def uppercase(str):
         if (ord(char) >= 97) and (ord(char) < 123):
             x = ord(char) - 32
             c.append(chr(x))
-            #print("{}".format(chr(x)), end="")
         else:
-            #print("{}".format(char), end="")
             c.append(char)
     h = ''.join(c)
     print(h)
