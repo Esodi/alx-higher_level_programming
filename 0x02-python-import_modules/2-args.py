1398b37b3c1ca1937553d8724652580ebfc8a754
Method Mkoma <gershommethod02@gmail.com>
2023-12-04 20:33:12 +0300

    Task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+par = sys.argv[1:]
+if (len(par) == 0):
+    print("{} arguments.".format(len(par)))
+elif (len(par) == 1):
+    print("{} argument:".format(len(par)))
+    print("{}: {}".format(len(par), sys.argv[1]))
+else:
+    print("{} arguments:".format(len(par)))
+    for i in range(1, (len(par) + 1)):
+        print("{}: {}".format(i, sys.argv[i]))
