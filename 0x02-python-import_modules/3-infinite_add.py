f9fc2a6ddb064ea74636ecaa26db83f65760d9a8
Method Mkoma <gershommethod02@gmail.com>
2023-12-04 20:54:20 +0300

    Task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,8 +2,8 @@
 if __name__ == "__main__":
     import sys
     args = sys.argv[1:]
-    l = len(args)
+    n = len(args)
     ans = 0
-    for i in range(1, (l + 1)):
+    for i in range(1, (n + 1)):
         ans = ans + int(sys.argv[i])
     print("{}".format(ans))
