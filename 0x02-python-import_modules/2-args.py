b18fe59264ab6347d115784fcac90b6662bed103
Method Mkoma <gershommethod02@gmail.com>
2023-12-04 20:34:59 +0300

    Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-import sys
-par = sys.argv[1:]
-if (len(par) == 0):
-    print("{} arguments.".format(len(par)))
-elif (len(par) == 1):
-    print("{} argument:".format(len(par)))
-    print("{}: {}".format(len(par), sys.argv[1]))
-else:
-    print("{} arguments:".format(len(par)))
-    for i in range(1, (len(par) + 1)):
-        print("{}: {}".format(i, sys.argv[i]))
+if __name__ == "__main__":
+    import sys
+    par = sys.argv[1:]
+    if (len(par) == 0):
+        print("{} arguments.".format(len(par)))
+    elif (len(par) == 1):
+        print("{} argument:".format(len(par)))
+        print("{}: {}".format(len(par), sys.argv[1]))
+    else:
+        print("{} arguments:".format(len(par)))
+        for i in range(1, (len(par) + 1)):
+            print("{}: {}".format(i, sys.argv[i]))
