caa009e528c23936535b035f51529b73a609ba05
Method Mkoma <gershommethod02@gmail.com>
2023-12-05 21:44:57 +0300

    Task 2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,5 +2,5 @@
 def replace_in_list(my_list, idx, element):
     if (idx < 0) or (idx > (len(my_list) - 0)):
         return None
-    my_list[idx] = element
+    my_list.insert(idx, element)
     return my_list
