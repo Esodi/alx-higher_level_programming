046e89c4641e17cdb7ea2006333eb1000f79d697
Method Mkoma <gershommethod02@gmail.com>
2023-12-05 22:05:37 +0300

    Task 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    a = len(my_list)
-    i = a
+    i = len(my_list)
     while i > 0:
         print("{:d}".format(i))
-        i-=1
+        i -= 1
