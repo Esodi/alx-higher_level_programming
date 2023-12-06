ddbf4ee8cd6dcecad33275df8c00bdb38cec64b5
Method Mkoma <gershommethod02@gmail.com>
2023-12-06 19:23:21 +0300

    Task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    if my_list is None:
+        return None
     lst = list(my_list)
-    if (idx < 0) or (idx > len(lst)):
+    if (idx < 0) or (idx > len(lst) - 1):
         return (lst)
     lst[idx] = element
     return (lst)
