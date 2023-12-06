10ef49a608c6b793e40bba9445860344716759eb
Method Mkoma <gershommethod02@gmail.com>
2023-12-06 20:31:39 +0300

    Task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if len(matrix) <= 1:
+    if matrix is None:
         print('{}'.format(''))
     for i in matrix:
+        if i is None:
+            print('')
         for j in range(0, len(i)):
             if j < len(i) - 1:
                 print("{:d}".format(i[j]), end=' ')
