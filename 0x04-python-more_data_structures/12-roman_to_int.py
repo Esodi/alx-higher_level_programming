2c7976547cfff3c9218c1e1bd456bdaaeb0b9128
Method Mkoma <gershommethod02@gmail.com>
2023-12-09 21:20:57 +0300

    Task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    d = {1000 : 'M', 500 : 'D', 100 : 'C', 50 : 'L', 10 : 'X', 5 : 'V', 1 : 'I'}
-    if not roman_string:
+    d = {1000: 'M', 500: 'D', 100: 'C', 50: 'L', 10: 'X', 5: 'V', 1: 'I'}
+    if not roman_string or not isinstance(roman_string, str):
         return 0
     s = []
     for i in roman_string:
         for k in d:
-            if i  == d[k]:
+            if i == d[k]:
                 s.append(k)
     n = 0
     c = 0
