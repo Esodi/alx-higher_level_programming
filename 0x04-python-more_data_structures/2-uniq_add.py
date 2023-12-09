459fe23e64edb44ff81a03e1e85863e837186b3f
Method Mkoma <gershommethod02@gmail.com>
2023-12-09 15:51:38 +0300

    Task 2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,6 +3,6 @@ def uniq_add(my_list=[]):
     my_list.sort()
     k = 0
     for i in range(len(my_list)):
-        if my_list[i] != my_list[i - 1]:
+        if i == 0 or my_list[i] != my_list[i - 1]:
             k = k + my_list[i]
     return k
