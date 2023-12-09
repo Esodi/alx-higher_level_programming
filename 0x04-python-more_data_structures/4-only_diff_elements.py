cc54400505c885d247575226e172ff702a0bbe51
Method Mkoma <gershommethod02@gmail.com>
2023-12-09 16:30:25 +0300

    Task 4

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -4,8 +4,8 @@ def only_diff_elements(set_1, set_2):
     s2 = list(set_2)
     for i in range(len(s1)):
         for j in range(len(s2)):
-            if s1[i] == s2[j]:
-                del s1[i]
-                del s2[j]
+            if s1[i - 1] == s2[j - 1]:
+                del s1[i - 1]
+                del s2[j - 1]
     k = s1 + s2
     return k
