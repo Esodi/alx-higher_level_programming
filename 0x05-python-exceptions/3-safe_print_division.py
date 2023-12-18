549ecefa855fe40a3efe81160c68c6ea2519e1b2
Method Mkoma <gershommethod02@gmail.com>
2023-12-18 22:26:58 +0300

    Task 3

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    try:
+        c = a / b
+    except ZeroDivisionError:
+        print('Inside result: None')
+        return None
+    finally:
+        if b / a != 0:
+            print("Inside result: {:.1f}".format(c))
+            return c
+        else:
+            return None
