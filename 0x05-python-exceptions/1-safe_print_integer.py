4973a573f42dcdca12d8652c3bb291ffb9c04d9b
Method Mkoma <gershommethod02@gmail.com>
2023-12-18 17:39:24 +0300

    Task 1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
 def safe_print_integer(value):
-    try:
-        print("{:d}".format(value))
-    except ValueError:
-        return False
-    else:
-        return True
+    if isinstance(value, int):
+        try:
+            print("{:d}".format(value))
+        except ValueError:
+            return False
+        else:
+            return True
