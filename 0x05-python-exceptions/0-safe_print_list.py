949ed473934bf90b2b04b50efd6c183cf86d9caf
Method Mkoma <gershommethod02@gmail.com>
2023-12-18 17:06:28 +0300

    Task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         for i in range(x):
             print("{}".format(my_list[i]), end='')
-            c+=1
+            c += 1
         print()
     except Exception:
         print()
