496eff7e337b0365b84a9ad93dfeafc654e2fb52
Method Mkoma <gershommethod02@gmail.com>
2023-12-19 20:13:13 +0300

    Task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
+"""A module created for square class.
+
+"""
+
 
 class Square:
+    """A class contains Square attributes.
+
+    """
     def __init__(self, size=0):
+        """A method that initialize class.
+           Args:
+                size (int): int value.
+
+        """
         if not isinstance(size, int):
             raise TypeError('size must be an integer')
         if size < 0:
