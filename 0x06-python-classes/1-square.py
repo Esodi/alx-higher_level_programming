8118f7e4b9dbc75db69a0fcad2ea8a039404e2f7
Method Mkoma <gershommethod02@gmail.com>
2023-12-19 20:10:00 +0300

    Task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python3
+"""The module created for square with initialization.
+
+"""
+
 
 class Square:
+    """A square class with size attribute.
+       Attributes:
+           size (int): One side of a square.
+
+    """
     def __init__(self, size):
+        """A method to initialize square class.
+           Args:
+               size (int): int value.
+
+        """
         self.__size = size
