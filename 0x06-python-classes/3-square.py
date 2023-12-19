8f86a5bb6ad0642073f764d19499c91107d465b7
Method Mkoma <gershommethod02@gmail.com>
2023-12-19 20:29:46 +0300

    Task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""A module that finds an area of a square
+
+"""
+
+
+class Square:
+    """A class that contain squae attributes.
+
+    """
+    def __init__(self, size=0):
+        """initialization method.
+
+        """
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        elif not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        else:
+            self.__size = size
+
+
+    def area(self):
+        """A method that calculate the area of the square.
+
+        """
+        return self.__size ** 2
