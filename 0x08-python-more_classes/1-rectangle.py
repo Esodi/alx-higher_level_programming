8aa3c30f28a78d1c9380dd7c599d05dab0b76d49
Method Mkoma <gershommethod02@gmail.com>
2024-01-07 17:23:46 +0300

    property fix

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -23,19 +23,19 @@ class Rectangle:
     @width.setter
     def width(self, value):
         if isinstance(value, int) == True:
-            if value < 0:
-                raise ValueError("width must be >= 0")
-            else:
+            if value >= 0:
                 self.__width = value
+            else:
+                raise ValueError("width must be >= 0")
         else:
             raise TypeError("width must be an integer")
 
     @height.setter
     def height(self, value):
         if isinstance(value, int) == True:
-            if value < 0:
-                raise ValueError("height must be >= 0")
-            else:
+            if value >= 0:
                 self.__height = value
+            else:
+                raise ValueError("height must be >= 0")
         else:
             raise TypeError("height must be an integer")
