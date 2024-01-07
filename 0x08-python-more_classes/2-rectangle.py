61f78a9bdd8fefe67893c85d45d261e75e0e9007
Method Mkoma <gershommethod02@gmail.com>
2024-01-07 17:53:43 +0300

    Task 02

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+    module that defines class rectangle.
+
+"""
+
+
+class Rectangle:
+    """rectangle class that giveout the perimeter and area."""
+
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if isinstance(value, int) is True:
+            if value >= 0:
+                self.__width = value
+            else:
+                raise ValueError("width must be >= 0")
+        else:
+            raise TypeError("width must be an integer")
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if isinstance(value, int):
+            if value >= 0:
+                self.__height = value
+            else:
+                raise ValueError("height must be >= 0")
+
+        else:
+            raise TypeError("height must be an integer")
+
+    def area(self):
+        return self.__height * self.__width
+
+    def perimeter(self):
+        return 2*(self.__height) + 2*(self.__width)
