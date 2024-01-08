7f68089f3b553a399168bb596985f70e566c1e2f
Method Mkoma <gershommethod02@gmail.com>
2024-01-08 22:00:58 +0300

    Task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -8,12 +8,8 @@
 def add_integer(a, b=98):
     """ The function to add two integers """
 
-    if isinstance(a, (int, float)):
-        a = int(a)
-    else:
+    if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
-    if isinstance(b, (int, float)):
-        b = int(b)
-    else:
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    return a + b
+    return int(a) + int(b)
