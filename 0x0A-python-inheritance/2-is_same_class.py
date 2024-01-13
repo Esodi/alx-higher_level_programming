a828502507cc3e0d99d02f0f63870ecc11946c60
Method Mkoma <gershommethod02@gmail.com>
2024-01-13 17:14:44 +0300

    Task 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" A Module to check type of instance"""
+
+
+def is_same_class(obj, a_class):
+    """ a function to check type of instance"""
+
+    return type(obj) is a_class
