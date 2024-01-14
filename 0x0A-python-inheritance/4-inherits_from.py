b8ef9b025f215c60461bc6d1ed951151ed62e25a
Method Mkoma <gershommethod02@gmail.com>
2024-01-14 16:35:23 +0300

    Task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -5,7 +5,4 @@
 def inherits_from(obj, a_class):
     """a function that returns True if the object is an instance of a class"""
 
-    return issubclass(type(obj), a_class) and type(obj) != a_class
-    #    """return True
-   # else:
-    #    return False"""
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
