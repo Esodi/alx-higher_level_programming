a2eaa99f05a23bdd379d9c25be665c00285e386c
Method Mkoma <gershommethod02@gmail.com>
2024-01-14 09:17:17 +0300

    Task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,6 +4,8 @@
 
 
 def write_file(filename="", text=""):
+    """function that writes a string to a text file (UTF8) """
+
     with open(filename, mode='w', encoding='utf-8') as f:
         f.write(text)
         return len(text)
