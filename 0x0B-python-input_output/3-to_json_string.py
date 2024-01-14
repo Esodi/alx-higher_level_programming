d75222737d001df6efd5e6301bdb962e70684f2d
Method Mkoma <gershommethod02@gmail.com>
2024-01-14 16:49:30 +0300

    Task 3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """a module with to_json_string() function"""
 import json
+
+
 def to_json_string(my_obj):
     '''function that returns the JSON representation '''
 
-    return json.dumps(my_obj) 
+    return json.dumps(my_obj)
