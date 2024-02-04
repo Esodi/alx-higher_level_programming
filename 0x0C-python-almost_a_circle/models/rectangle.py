115704d7c6d6bca4c98c1c47f861cb45e3ed1d6d
Method Mkoma <gershommethod02@gmail.com>
2024-02-04 16:50:12 +0300

    Task 6: add __str__() method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -97,3 +97,7 @@ class Rectangle(Base):
         '''method that print rectangle using #'''
         for i in range(self.__height):
             print("#"*self.__width)
+
+    def __str__(self):
+        '''__str__ method'''
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height)
