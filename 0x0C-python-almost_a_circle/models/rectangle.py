3f157b0ef3788da24b07deddd25cf0c5bfa63420
Method Mkoma <gershommethod02@gmail.com>
2024-02-04 17:09:22 +0300

    Task 6: update display() method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -95,8 +95,9 @@ class Rectangle(Base):
 
     def display(self):
         '''method that print rectangle using #'''
+        print("\n"*self.__y, end='')
         for i in range(self.__height):
-            print("#"*self.__width)
+            print(" "*self.__x + "#"*self.__width)
 
     def __str__(self):
         '''__str__ method'''
