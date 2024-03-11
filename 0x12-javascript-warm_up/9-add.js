309cb7fba1db9a566b70dcb7fc16f254d0474ce1
Method Mkoma <gershommethod02@gmail.com>
2024-03-12 01:11:44 +0300

    Task 9

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+function add(a, b) {
+  const c = a + b;
+  console.log(c);
+}
+
+const a = process.argv[2];
+const b = process.argv[3];
+const aInt = parseInt(a);
+const bInt = parseInt(b);
+
+add(aInt, bInt);
