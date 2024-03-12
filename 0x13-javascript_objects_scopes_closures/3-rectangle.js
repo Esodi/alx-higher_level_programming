e8aef614c3769ccbc9893b03769cef5c4a6b1f3b
Method Mkoma <gershommethod02@gmail.com>
2024-03-12 12:12:23 +0300

    Task 3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,26 +1,23 @@
 #!/usr/bin/node
 
-class Rectangle
-{
-	constructor (w, h)
-	{
-		if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(w))
-		{
-			return (null);
-		}
-		this.width = w;
-		this.height = h;
-	}
-	print ()
-	{
-		let r = '';
-		for (let i = 0; i < this.width; i++)
-		{
-			r += 'X';
-		}
-		for (let j = 0; j < this.height; j++)
-		{
-			console.log(r);
-		}
-	}
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(w)) {
+      return (null);
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    let r = '';
+    for (let i = 0; i < this.width; i++) {
+      r += 'X';
+    }
+    for (let j = 0; j < this.height; j++) {
+      console.log(r);
+    }
+  }
 }
+
+module.exports = Rectangle;
