9f7639a472afadf9c014c55bf81213812a2faadf
Method Mkoma <gershommethod02@gmail.com>
2024-03-17 20:21:01 +0300

    task 5: fix

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -11,11 +11,11 @@ def allcitiesv2(username, password, dbname, state_name):
     '''a function that processes an output'''
     db = MySQLdb.connect(host='localhost', user=username, port=3306, db=dbname, passwd=password)
     cur = db.cursor()
-    sql = "SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id = states.id WHERE states.name = %s ORDER BY cities.id ASC"
+    sql = "SELECT states.name FROM cities INNER JOIN states ON cities.state_id = states.id WHERE states.name = %s ORDER BY cities.id ASC"
     cur.execute(sql, (state_name,))
     fetch = cur.fetchall()
     for i in fetch:
-        print('{},'.format(i[1]), end='')
+        print('{}'.format(i))
     cur.close()
     db.close()
 
