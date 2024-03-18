56d33413b674126d61b72830ac33097e23821472
Method Mkoma <gershommethod02@gmail.com>
2024-03-18 10:48:05 +0300

    Task 1: fixing

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -19,7 +19,7 @@ def list_Name(username, password, dbname, state_name):
             db=dbname
             )
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = %s", (state_name,))
+    cur.execute("SELECT * FROM states WHERE name = {}".format(state_name,))
     fetch = cur.fetchall()
     for i in fetch:
         print(f'{i}')
