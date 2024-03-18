33b3743732a2078538fa0a5aa2884f8a68484b02
Method Mkoma <gershommethod02@gmail.com>
2024-03-18 11:10:15 +0300

    Task 4: fixing

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -18,13 +18,12 @@ def allcities(username, password, database):
             db=database
             )
     cur = db.cursor()
-    cur.execute("
-                SELECT cities.id, cities.name, states.name
-                FROM cities
-                INNER JOIN states
-                ON cities.state_id=states.id
-                ORDER BY cities.id
-                ASC"
+    cur.execute("SELECT cities.id, cities.name, states.name "
+                "FROM cities "
+                "INNER JOIN states "
+                "ON cities.state_id=states.id "
+                "ORDER BY cities.id "
+                "ASC"
                 )
     fetch = cur.fetchall()
     for i in fetch:
