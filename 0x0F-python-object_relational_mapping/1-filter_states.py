d4d49aa908a4fe3e22989fae126d5e12a7f246a3
Method Mkoma <gershommethod02@gmail.com>
2024-03-22 20:32:08 +0300

    Task 1: Fixing

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -19,13 +19,15 @@ def list_Ns(username, password, dbname):
             db=dbname
             )
     cur = db.cursor()
-    sql = ("SELECT * "
-    "FROM states "
-    "WHERE name "
-    "LIKE 'N%' "
-    "COLLATE utf8mb4_bin "
-    "ORDER BY states.id "
-    "ASC")
+    sql = (
+            "SELECT * "
+            "FROM states "
+            "WHERE name "
+            "LIKE 'N%' "
+            "COLLATE utf8mb4_bin "
+            "ORDER BY states.id "
+            "ASC"
+    )
     cur.execute(sql)
     fetchedNs = cur.fetchall()
     for i in fetchedNs:
