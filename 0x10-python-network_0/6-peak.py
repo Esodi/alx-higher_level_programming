789fb919ac71fc4b7e6fe41e5c3ebc6d433f58c8
Method Mkoma <gershommethod02@gmail.com>
2024-03-29 23:31:57 +0300

    Task 6: Finding a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,8 +4,7 @@
 
 def find_peak(list_of_integers):
     """ finds a peak in a list of unsorted integers."""
-    lst = sorted(list_of_integers)
-    if len(lst) != 0:
-        return lst[-1]
-    else:
-        return None
+    list_of_integers.sort()
+    if len(list_of_integers) != 0:
+        return list_of_integers[-1]
+    return None
