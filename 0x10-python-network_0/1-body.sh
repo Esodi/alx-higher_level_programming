8164f12eb136a7fa35ad90be77e9fd11199d1678
Method Mkoma <gershommethod02@gmail.com>
2024-03-29 20:58:47 +0300

    Task 1: Fixing

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a GET request to the URL, and displays the body.
-curl -sI http://"$1" | grep -q 200 && curl -s http://"$1"
+curl -si -o response.txt -w "%{http_code}" "$1" && [ "$(cat response.txt)" = "200" ] && curl -s "$1"
+
