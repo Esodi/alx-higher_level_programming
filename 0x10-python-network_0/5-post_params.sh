77b92a604c28c60f4d4badd893e83e132afb1815
Method Mkoma <gershommethod02@gmail.com>
2024-03-29 21:57:24 +0300

    Task 5:  cURL POST parameters

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a POST request to the passed URL, and displays the body of the response.
-curl -s -X POST -d "email: test@gmail.com" http://"$1"
+curl -s -X POST -H "Content-Type: application/json" -d '{"email": "test@gmail.com"}' http://"$1"
